2377c2e46dfbbd0005e41c4536417b0be3dc1f0f
Subhamdas20 <subham21.sd@gmail.com>
2021-11-26 10:58:42 +0530

    first commit

---
+++ src/main/java/com/bridgelabz/ConnectSQL.java
@@ -0,0 +1,31 @@
+package com.bridgelabz;
+
+import java.sql.*;
+
+public class ConnectSQL {
+
+    public static void main(String[] args) {
+        String jdbcurl="jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+        String userName="root";
+        String password="SubhamDas@20";
+        Connection con;
+        Statement statement ;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver Loaded");
+            con= DriverManager.getConnection(jdbcurl,userName,password);
+            statement= con.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from employee_payroll");
+            while(resultSet.next()) {
+            String name = resultSet.getString("name");
+            System.out.println(name);
+            int salary = resultSet.getInt("salary");
+            System.out.println(salary);
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
